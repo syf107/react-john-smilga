bb6216e2b73b0f594b9a257c0168b467d5d4b722
syf107 <syariefarifin107@gmail.com>
2022-04-02 23:49:40 +0700

    first commit for john smilga's react course.

---
+++ tutorial/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// stateless functional component
+// always return jsx
+function Greeting() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hello world')
+// }
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
