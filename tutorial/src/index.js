c7808aa15cd615b0632bd14f57e713e836694d75
syf107 <syariefarifin107@gmail.com>
2022-04-05 00:27:24 +0700

    more react john smilga. Learning about CSS in JS, JavaScript in JSX, and also Props

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,44 +1,51 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+// CSS
 import './index.css';
 
-// Nested Components, React Tools.
+// setup vars
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+};
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/912w0zCVe-L._AC_UL200_SR200,200_.jpg',
+  title: 'Antiracist Baby Picture Book',
+  author: 'Ibram X. Kendi',
+};
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className='book'>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
 
-const Image = () => {
-  return (
-    <img
-      src='https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg'
-      alt=''
-    />
-  );
-};
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4>Amelia Hepworth</h4>;
-
-ReactDom.render(<BookList />, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(<BookList />);
 
 // import React from 'react';
 // import ReactDom from 'react-dom';
