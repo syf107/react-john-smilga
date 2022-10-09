a92fed27a4333a88b2af1720e2cde3728b5a82c7
syf107 <syariefarifin107@gmail.com>
2022-10-09 22:54:35 +0700

    adding cocktail and markdown project

---
+++ 3-react-projects/15-cocktails/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo neque esse
+        quas nobis eaque animi? Modi, voluptatem unde. Recusandae magnam eum
+        odit, ipsa ducimus quam voluptates quo cumque soluta iste temporibus.
+        Culpa blanditiis quibusdam harum nesciunt non commodi deleniti natus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
